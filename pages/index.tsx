098cb71ea10f07cdd5c3f78b65254cc5f11cdf76
Joe <joimee.cajandab@gmail.com>
2022-02-19 19:02:02 +0800

    ⚡️ Page setups, root style updates, root configs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,9 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Layout from '../components/layout'
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
+const Home = () => (
+  <Layout title="Bayanihan">
+    Bayanihan
   </Layout>
 )
 
-export default IndexPage
+export default Home
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,19 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
+  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
     extend: {},
   },
   plugins: [],
-};
+}
