ba9abee556bdf3069fa8d8771c372b6c23f8cc51
Joe <joimee.cajandab@gmail.com>
2022-02-21 07:08:51 +0800

    ⚡️ Beautify header

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -4,14 +4,14 @@ import styles from './header.module.scss'
 
 const Header = () => (
   <header className={cn('container py-10')}>
-    <nav className={cn('flex justify-between items-center')}>
-      <Link href="/">
+    <nav className={cn(styles.nav, 'flex justify-between items-center')}>
+      <Link href="/about">
         <a>About</a>
       </Link>
-      <Link href="/about">
+      <Link href="/">
         <a className={styles.brand}>Bayanihan</a>
       </Link>
-      <Link href="/about">
+      <Link href="/register">
         <a>Register</a>
       </Link>
     </nav>
